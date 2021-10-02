7dfd2ca5c0b55491bd1271b7281b93883057ffb3
unhurried <unhurried@users.noreply.github.com>
2021-10-02 16:35:15 +0900

    Add an example of AOP.

--- backend/src/main/kotlin/backend/aspect/LoggerAspect.kt
+++ backend/src/main/kotlin/backend/aspect/LoggerAspect.kt
@@ -6,11 +6,14 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 
+/** An aspect (Spring AOP) that logs HTTP method and path of API calls. */
+// @Aspect and @Component are required to register an aspect.
 @Aspect
 @Component
 class LoggerAspect (val request: HttpServletRequest) {
     private val logger = LoggerFactory.getLogger(LoggerAspect::class.java)
 
+    // @Before: Execute an advice before a join point.
     @Before("within(backend.resource.ToDoResource)")
     public fun before() {
         logger.info("API call: " + request.method + " " + request.pathInfo);
