cca3b7dc7cf6370b3e1828e1eb183bb629adacd6
unhurried <unhurried@users.noreply.github.com>
2021-10-05 07:01:42 +0900

    Add an example of unit test.

--- backend/src/main/kotlin/backend/gui/Browser.kt
+++ backend/src/main/kotlin/backend/gui/Browser.kt
@@ -1,8 +1,10 @@
 package backend.gui
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext
+import org.springframework.context.ApplicationContext
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import org.springframework.web.util.UriComponentsBuilder
@@ -13,18 +15,26 @@ import java.io.IOException
 @Component
 class Browser : CommandLineRunner {
     @Autowired
-    private lateinit var context: ServletWebServerApplicationContext
+    private lateinit var ctx: ApplicationContext;
 
     @Autowired
     private lateinit var environment: Environment
 
     @Throws(IOException::class)
     override fun run(vararg args: String) {
+        // Skip opening the default browser when servlet is not running.
+        var sctx: ServletWebServerApplicationContext
+        try {
+             sctx = ctx.getBean(ServletWebServerApplicationContext::class.java)
+        } catch(e: NoSuchBeanDefinitionException) {
+            return;
+        }
+
         if (!listOf(*environment.activeProfiles).contains("development")) {
             val desktop = Desktop.getDesktop()
             val uri = UriComponentsBuilder
                     .fromUriString("http://localhost/")
-                    .port(context.webServer.port)
+                    .port(sctx.webServer.port)
                     .build().toUri()
             desktop.browse(uri)
         }
